a8db0f7ec52752a9c77f6e72419591b3488bb52c
gabriel <gabrielmussolini@hotmail.com>
2020-09-02 13:53:55 -0300

    ci: Setup Workflow

--- packages/admin-panel/src/server.ts
+++ packages/admin-panel/src/server.ts
@@ -6,4 +6,4 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3333, () => console.log('Server running at port 3333'));
+app.listen(3333, () => console.log('Server russasdnning at port 3333aa'));
