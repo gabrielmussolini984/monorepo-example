12e6d8bb8503e9da3d82a299f2b4377d75a17526
gabriel <gabrielmussolini@hotmail.com>
2020-09-03 21:27:52 -0300

    teste

--- packages/admin-panel/src/server.ts
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(3333, () => console.log('Server russasdnning at port 3333aa'));
