4149e8b51d07e7811d919ca4a691a6395617f4c5
gabriel <gabrielmussolini@hotmail.com>
2020-09-04 07:21:25 -0300

    teste

--- packages/admin-panel/src/shared/infra/http/server.ts
+++ packages/admin-panel/src/shared/infra/http/server.ts
@@ -6,6 +6,6 @@ const connect = async () => {
   const { app } = new App();
   const port = process.env.PORT || 3000;
   // eslint-disable-next-line
-  app.listen(port, () => console.log('Server Runnings at port 3000'));
+  app.listen(port, () => console.log('Server Running at port 3000'));
 };
 connect();
